bfb2b68b44e37bbe85ec6e803489b2d9659c51df
Eric Satterwhite <eric.satterwhite@eagles.usm.edu>
2017-11-13 13:05:01 -0600

    fixed bug with testconfig.tests.json that was preventing test files from being linted. Added base testing file to make sure tests are working

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,2 +1,10 @@
 import { expect } from 'chai';
 import 'mocha';
+
+describe('Testing', () => {
+    it ('should run tests', () => {
+        expect(1).to.equal(1);
+        expect(0).to.not.equal(1);
+    });
+});
+
