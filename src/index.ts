ab2b35103c3480aaa8a8233dc89555b08fca26ac
Eric <eric.satterwhite@eagles.usm.edu>
2017-11-18 17:57:46 -0600

    testing coveralss threshold

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
+if (1) {
+    console.log('testing code coverage threshold');    
+} else {
+    console.log('testing code coverage threshold');
+}
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
+console.log('testing code coverage threshold');
