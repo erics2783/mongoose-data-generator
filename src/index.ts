c30174b10d85ee70ecb7a929a25536b5fcac57f9
Eric Satterwhite <eric.satterwhite@eagles.usm.edu>
2017-11-18 18:53:00 -0600

    closes #1

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export const coverallsTest = (): void => {
     console.log('testing code coverage threshold');
     console.log('testing code coverage threshold');
     console.log('testing code coverage threshold');
+    console.log('testing code coverage threshold');
     if (1) {
         console.log('testing code coverage threshold');
     } else {
